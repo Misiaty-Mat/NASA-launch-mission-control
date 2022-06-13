e18e5a6ebf79b716af2b41329cbb14541c4488c2
Misiaty-Mat <m.a.s.m@interia.pl>
2022-06-13 13:52:43 +0200

    Added POST and DELETE launches endpoint

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,9 +1,54 @@
-const { getAllLaunches } = require("../../models/launches.model");
+const {
+  getAllLaunches,
+  addNewLaunch,
+  existsLaunchWithId,
+  abortLaunchById,
+} = require("../../models/launches.model");
 
-const HttpGetAllLaunches = (req, res) => {
+const httpGetAllLaunches = (req, res) => {
   return res.status(200).json(getAllLaunches());
 };
 
+const httpAddNewLaunch = (req, res) => {
+  const launch = req.body;
+
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.target
+  ) {
+    return res.status(400).json({
+      error: "Missing required data",
+    });
+  }
+
+  launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: "Ivalid date entered",
+    });
+  }
+
+  addNewLaunch(launch);
+  return res.status(201).json(launch);
+};
+
+const httpAbortLaunch = (req, res) => {
+  const launchId = Number(req.params.id);
+
+  if (!existsLaunchWithId(launchId)) {
+    return res.status(404).json({
+      error: "No launch with given id found",
+    });
+  }
+
+  const aborted = abortLaunchById(launchId);
+  return res.status(200).json(aborted);
+};
+
 module.exports = {
-  HttpGetAllLaunches,
+  httpGetAllLaunches,
+  httpAddNewLaunch,
+  httpAbortLaunch,
 };
--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -1,9 +1,15 @@
 const express = require("express");
 
-const { HttpGetAllLaunches } = require("./launches.controller");
+const {
+  httpGetAllLaunches,
+  httpAddNewLaunch,
+  httpAbortLaunch,
+} = require("./launches.controller");
 
 const launchesRouter = express.Router();
 
-launchesRouter.get("/launches", HttpGetAllLaunches);
+launchesRouter.get("/", httpGetAllLaunches);
+launchesRouter.post("/", httpAddNewLaunch);
+launchesRouter.delete("/:id", httpAbortLaunch);
 
 module.exports = launchesRouter;
